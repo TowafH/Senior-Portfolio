d73564a6a97df5e7207f17df680d3f28303e7e58
Towaf H <towafh323@gmail.com>
2025-12-10 20:09:08 -0500

    Update Headshot

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
             {/* grid grid-cols-1 can be changed to grid grid-cols-2 to include certification */}
             <div className="grid grid-cols-1 items-center gap-x-5 p-3">
                 <div className="flex justify-center items-center gap-x-5">
-                    <img src="./Headshot.jpg" className="w-50 h-auto rounded-full" />
+                    <img src="./Headshot.jpg" className="w-40 h-auto rounded-full" />
                     <h1 className="w-150">
                     Towaf is a senior in the Web/App Development CTE Track at Thomas A. Edison Career & Technical Education High School. 
                     He is passionate about computer science, web development, and IT, he is actively expanding his skills by building projects with React, Python, Java, and C#
